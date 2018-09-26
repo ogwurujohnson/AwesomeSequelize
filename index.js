3715b53b1813bc652673cc8296d50402bf3e2a33
blackhatjohnny <ogwurujohnson@gmail.com>
2018-09-26 13:02:22 +0100

    created endpoints for users get and view

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+app.post('/api/users', (req, res) => {
+  User.create(req.body)
+    .then(user => res.json(user))
+});
+
+app.get('/api/users', (req, res) => {
+  User.findAll().then(users => res.json(users))
+});
+
 const port = 3000
 app.listen(port, () => {
   console.log(`Running on http://localhost:${port}`);

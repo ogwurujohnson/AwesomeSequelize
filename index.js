d8e00450e1de0cf117ac29a9ba8dd86d19ef63f7
blackhatjohnny <ogwurujohnson@gmail.com>
2018-09-26 13:32:19 +0100

    major endpoints created

--- index.js
+++ index.js
@@ -16,6 +16,40 @@ app.get('/api/users', (req, res) => {
   User.findAll().then(users => res.json(users))
 });
 
+app.post('/api/blogs', (req, res) => {
+  const body = req.body;
+  const tags = body.tags.map(tag => Tag.findOrCreate({ where: { name: tag.name }, defaults: { name: tag.name } }).spread((tag, created) => tag))
+
+  User.findById(body.userId)
+    .then(() => Blog.create(body))
+    .then(blog => Promise.all(tags).then(storedTags => blog.addTags(storedTags)). then(() => blog))
+    .then(blog => Blog.findOne({ where: {id: blog.id}, include: [User, Tag]}))
+    .then(blogWithAssociations => res.json(blogWithAssociations))
+    .catch(err => res.status(400).json({ err: `User with id = [${body.userId}] doesn\'t exist.`}))
+});
+
+app.get('/api/blogs/:userId?', (req, res) => {
+  let query;
+  if(req.params.userId) {
+    query = Blog.findAll({ include: [
+      { model: User, where: { id: req.params.userId } },
+      { model: Tag }
+    ]});
+  } else {
+    query = Blog.findAll({ include: [Tag, User] })
+  }
+  return query.then(blogs => res.json(blogs))
+});
+
+app.get('/api/blogs/:tag/tag', (req, res) => {
+  Blog.findAll({
+    include: [
+      { model: Tag, where: { name: req.params.tag } }
+    ]
+  })
+    .then(blogs => res.json(blogs))
+});
+
 const port = 3000
 app.listen(port, () => {
   console.log(`Running on http://localhost:${port}`);
